13ba0d09f16f48f4dc3a759f0af0ef861e699d4c
Mark Chmarny <mark@chmarny.com>
2019-05-12 16:39:49 -0700

    cpu load

--- data.go
+++ data.go
@@ -15,51 +15,41 @@ import (
 	"github.com/shirou/gopsutil/mem"
 )
 
-var (
-	memStats  *mem.SwapMemoryStat
-	cpuStats  cpu.InfoStat
-	loadStats *load.AvgStat
-)
-
-func init() {
+func makeEvent(min, max float64) string {
 
 	ms, err := mem.SwapMemory()
 	failOnErr(err)
-	memStats = ms
 
-	cs, err := cpu.Info()
+	d, err := time.ParseDuration("1s")
 	failOnErr(err)
-	cpuStats = cs[0]
+	cs, err := cpu.Percent(d, false)
+	failOnErr(err)
 
 	ls, err := load.Avg()
 	failOnErr(err)
-	loadStats = ls
-}
-
-func makeEvent(min, max float64) string {
 
 	event := struct {
-		SourceID    string    `json:"source_id"`
-		EventID     string    `json:"event_id"`
-		EventTs     time.Time `json:"event_ts"`
-		Label       string    `json:"label"`
-		MemFree     float64   `json:"memFree"`
-		CPUFree     float64   `json:"cpuFree"`
-		LoadAvg1    float64   `json:"loadAvg1"`
-		LoadAvg5    float64   `json:"loadAvg5"`
-		LoadAvg15   float64   `json:"loadAvg15"`
-		RandomValue float64   `json:"randomValue"`
+		SourceID     string    `json:"source_id"`
+		EventID      string    `json:"event_id"`
+		EventTs      time.Time `json:"event_ts"`
+		Label        string    `json:"label"`
+		MemUsed      float64   `json:"mem_used"`
+		CPUUsed      float64   `json:"cpu_used"`
+		Load1        float64   `json:"load_1"`
+		Load5        float64   `json:"load_5"`
+		Load15       float64   `json:"load_15"`
+		RandomMetric float64   `json:"random_metric"`
 	}{
-		SourceID:    *eventSrc,
-		EventID:     fmt.Sprintf("%s-%s", idPrefix, uuid.NewV4().String()),
-		EventTs:     time.Now().UTC(),
-		Label:       *metricLabel,
-		RandomValue: min + rand.Float64()*(max-min),
-		MemFree:     memStats.UsedPercent,
-		CPUFree:     cpuStats.Mhz,
-		LoadAvg1:    loadStats.Load1,
-		LoadAvg5:    loadStats.Load5,
-		LoadAvg15:   loadStats.Load15,
+		SourceID:     *eventSrc,
+		EventID:      fmt.Sprintf("%s-%s", idPrefix, uuid.NewV4().String()),
+		EventTs:      time.Now().UTC(),
+		Label:        *metricLabel,
+		RandomMetric: min + rand.Float64()*(max-min),
+		MemUsed:      ms.UsedPercent,
+		CPUUsed:      cs[0],
+		Load1:        ls.Load1,
+		Load5:        ls.Load5,
+		Load15:       ls.Load15,
 	}
 
 	data, _ := json.Marshal(event)
